0add3e88011da69e69faebc128113607bc7f39fe
Ryan Park <nbiosupr@gmail.com>
2018-11-12 23:30:00 +0800

    Fix the order of loading middle ware

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
-var router = require('./router/main')(app);
+
 
 app.set('views', __dirname + '/views' ); //__dirname = Environment Variable
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000");
-});
 app.use(express.static(__dirname+'/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+var router = require('./router/main')(app);
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000");
+});
