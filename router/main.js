6dca2cf0372856fa4c40918dec00e0a153d3d2a7
Ryan Park <nbiosupr@gmail.com>
2018-11-12 23:07:03 +0800

    Add code to test parsing

--- router/main.js
+++ router/main.js
@@ -1,11 +1,14 @@
 module.exports = function(app){
   app.get('/', function(req,res){
-    res.render('index.html')
+    res.render('index.html');
   });
   app.get('/newpost', (req,res)=>{
-    res.render('newpost.html')
+    res.render('newpost.html');
   });
-  app.put('/newpost', (req,res)=>{
-    //todo
+  app.post('/newpost', (req,res)=>{
+    var title = req.body.title;
+    var content = req.body.postContent;
+    console.log('title: ' + title);
+    console.log('content: ' + content);
   });
 }
