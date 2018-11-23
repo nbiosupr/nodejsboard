1837a16331fddc7ffb5c21823400f03bd648825f
Ryan Park <nbiosupr@gmail.com>
2018-11-23 13:08:36 +0000

    Add code for distributing middleware function

---
+++ middleware/crud.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+//config
+const path = `public/post`;
+
+module.exports = function(){
+  
+    function loadPostList(){
+        var postList = fs.readdirSync(path);
+        return postList;
+    }
+  
+    function creatPost(req, res){
+        var title = req.body.title;
+        var content = req.body.postContent;
+
+        fs.writeFile(`${path}/${title}.txt`, content, 'utf8', function(err){
+            res.redirect(301, '/');
+        });
+    }
+    
+    function readPost(req, res){
+        
+    }
+    
+    function updatePost(req, res){
+        
+    }
+    
+    function deletePost(req, res){
+        
+    }
+};
