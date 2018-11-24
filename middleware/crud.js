563236d1178090f67d23728cf8620ece9bcf702c
Ryan Park <nbiosupr@gmail.com>
2018-11-24 04:02:46 +0000

    Fix code for module distributing

--- middleware/crud.js
+++ middleware/crud.js
@@ -3,31 +3,30 @@ const fs = require('fs');
 //config
 const path = `public/post`;
 
-module.exports = function(){
-  
-    function loadPostList(){
-        var postList = fs.readdirSync(path);
-        return postList;
-    }
-  
-    function createPost(req, res){
-        var title = req.body.title;
-        var content = req.body.postContent;
-
-        fs.writeFile(`${path}/${title}.txt`, content, 'utf8', function(err){
-            res.redirect(301, '/');
-        });
-    }
-    
-    function readPost(req, res){
-        
-    }
-    
-    function updatePost(req, res){
-        
-    }
-    
-    function deletePost(req, res){
-        
-    }
+exports.loadPostList = function(){
+    var postList = fs.readdirSync(path);
+    //console.log(postList);
+    return postList;
 };
+
+exports.createPost = function(req, res){
+    var title = req.body.title;
+    var content = req.body.postContent;
+
+    fs.writeFile(`${path}/${title}.txt`, content, 'utf8', function(err){
+        res.redirect(301, '/');
+    });
+};
+
+exports.readPost = function(req, res){
+
+};
+
+exports.updatePost = function(req, res){
+
+};
+
+exports.deletePost = function(req, res){
+
+};
+
