d7b4c0a3055264f29b827f132ebcf10023d5f567
Ryan Park <nbiosupr@gmail.com>
2018-11-25 13:59:37 +0000

    Add router for updating post

--- router/main.js
+++ router/main.js
@@ -5,13 +5,24 @@ module.exports = function(app){
     app.get('/', function(req,res){
         res.render('index.ejs', {postList : crud.loadPostList()});
     });
+    //Route for creating post
     app.get('/newpost', (req,res)=>{
         res.render('newpost.ejs');
     });
     app.post('/newpost', (req,res)=>{
         crud.createPost(req,res);
     });
+    
+    //Route for reading post
     app.get('/readpost', (req,res)=>{
         crud.readPost(req,res); 
+    });
+    
+    //Route for updating post
+    app.get('/updatepost', (req,res)=>{
+        crud.updatePost(req,res);
+    });
+    app.post('/updatepost', (req,res)=>{
+        
     });
 };
